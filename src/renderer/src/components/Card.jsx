45b3df5660fdd5b5954d2eda2e3a381c3134aa97
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2024-04-09 13:04:59 +1000

    Create the card component

---
+++ src/renderer/src/components/Card.jsx
@@ -0,0 +1,5 @@
+import '../assets/css/card.css'
+
+export default function Card({ children }) {
+  return <div className="card">{children}</div>
+}
--- src/renderer/src/pages/Clients.jsx
+++ src/renderer/src/pages/Clients.jsx
@@ -1,9 +1,13 @@
+import Card from '../components/Card'
+
 export default function Clients() {
   // const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <h1>Clients</h1>
+      <Card>
+        <h1>Client view</h1>
+      </Card>
     </>
   )
 }
