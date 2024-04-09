24763b9f0ce978b2fe51753601a5754b800cde45
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2024-04-09 16:11:55 +1000

    Finally got the form to work

--- src/renderer/src/pages/ClientForm.jsx
+++ src/renderer/src/pages/ClientForm.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
+import '../assets/css/form.css'
+
 export default function ClientForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    dob: '',
+    participantNumber: ''
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // next line is just for testing
+    console.log(formData)
+  }
+
   return (
-    <form>
-      <fieldset>
-        <label htmlFor="name">Name</label>
-        <input name="name" type="text" />
-      </fieldset>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="name">Name</label>
+          <input name="name" type="text" value={formData.name} onChange={handleInputChange} />
+
+          <label htmlFor="dob">Date of birth</label>
+          <input name="dob" type="date" value={formData.dob} onChange={handleInputChange} />
+
+          <label htmlFor="participantNumber">participantNumber</label>
+          <input
+            name="participantNumber"
+            type="text"
+            value={formData.participantNumber}
+            onChange={handleInputChange}
+          />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
