214b8200fc9db872a4e02d0ddb13b35e24f7c915
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2024-04-09 13:54:58 +1000

    Made clients autoload and reworked the flexbox

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,18 +1,18 @@
 import { HashRouter, Route, Routes, Link } from 'react-router-dom'
 import './assets/css/app.css'
 
-import Clients from './pages/Clients'
-import Invoices from './pages/Invoices'
-
 import person from './assets/svg/person.svg'
 import receipt from './assets/svg/receipt.svg'
+
+import Clients from './pages/Clients'
+import Sessions from './pages/Sessions'
 
 export default function App() {
   return (
     <div className="wrapper">
       <HashRouter>
         <nav>
-          <Link to="/clients">
+          <Link to="/">
             <img className="icon" src={person} width="36px" height="36px" />
           </Link>
           <Link to="/invoices">
@@ -21,8 +21,8 @@ export default function App() {
         </nav>
         <section>
           <Routes>
-            <Route path="/clients" element={<Clients />} />
-            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/" element={<Clients />} />
+            <Route path="/invoices" element={<Sessions />} />
           </Routes>
         </section>
       </HashRouter>
