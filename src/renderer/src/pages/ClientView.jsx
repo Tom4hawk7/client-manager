46c4f1bd62ec1929cf2301cfe32f836db87da87e
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2024-04-10 10:58:18 +1000

    Cleaning up unnecessary code

--- src/renderer/src/pages/ClientView.jsx
+++ src/renderer/src/pages/ClientView.jsx
@@ -3,18 +3,12 @@ import addClient from '../assets/svg/person-add.svg'
 import { Link } from 'react-router-dom'
 
 export default function ClientView() {
-  // const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
   return (
     <>
       <Card>
         <Link to="/client-form">
           <img src={addClient} width="36" height="36" />
         </Link>
-      </Card>
-      <Card>
-        <button onClick={ipcHandle}>Send ping</button>
       </Card>
     </>
   )
