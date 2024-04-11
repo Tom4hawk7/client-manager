cd6115a33b4f602d7f79cc872ac2f79e22de0de6
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2024-04-12 09:59:35 +1000

    Refactored the database code

--- src/main/database.js
+++ src/main/database.js
@@ -1,21 +1,31 @@
+import { insertOperations, insertOperationsKeys } from './insert'
 import Database from 'better-sqlite3'
 import * as insert from './insert'
 
 const db = new Database('src\\client-manager.db')
 
-function appendLastInsertID(object, property) {
-  const id = db.prepare(`SELECT LAST_INSERT_ROWID()`).get()
-  object[property] = id['LAST_INSERT_ROWID()']
+// helper functions
+function executeOperations(clientData, operations) {
+  operations.forEach((operation) => {
+    db.prepare(operation).run(clientData)
+  })
 }
 
+// insert helper functions
+function appendLastInsertId(clientData, property) {
+  const id = db.prepare(`SELECT LAST_INSERT_ROWID()`).get()
+  clientData[property] = id['LAST_INSERT_ROWID()']
+}
+
+function executeOperationKeys(clientData, operations) {
+  for (const key in operations) {
+    db.prepare(operations[key]).run(clientData)
+    appendLastInsertId(clientData, key)
+  }
+}
+
+// main database operations
 export function createClient(_event, clientData) {
-  db.prepare(insert.client).run(clientData)
-  appendLastInsertID(clientData, 'client_id')
-
-  db.prepare(insert.plan_manager).run(clientData)
-  appendLastInsertID(clientData, 'plan_manager_id')
-
-  db.prepare(insert.address).run(clientData)
-  db.prepare(insert.parent).run(clientData)
-  db.prepare(insert.plan).run(clientData)
+  executeOperationKeys(clientData, insertOperationsKeys)
+  executeOperations(clientData, insertOperations)
 }
--- src/main/insert.js
+++ src/main/insert.js
@@ -1,18 +1,21 @@
-export const client =
+const client =
   'INSERT INTO Client (name, dob, participant_number) ' +
   'VALUES (@client_name, @dob, @participant_number)'
 
-export const address =
+const address =
   'INSERT INTO Address (street, suburb, state, code, client_id) ' +
   'VALUES (@street, @suburb, @state, @code, @client_id)'
 
-export const parent =
+const parent =
   'INSERT INTO Parent (name, email, phone, client_id) ' +
   'VALUES (@parent_name, @parent_email, @phone, @client_id)'
 
-export const plan_manager =
+const plan_manager =
   'INSERT INTO PlanManager (name, email) ' + 'VALUES (@plan_manager_name, @plan_manager_email)'
 
-export const plan =
+const plan =
   'INSERT INTO Plan (start_date, end_date, budget, plan_manager_id, client_id) ' +
   'VALUES (@start_date, @end_date, @budget, @plan_manager_id, @client_id)'
+
+export const insertOperations = [address, parent, plan]
+export const insertOperationsKeys = { client_id: client, plan_manager_id: plan_manager }
