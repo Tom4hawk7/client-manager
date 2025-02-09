9c472b5df13d372d82f41e0ebeba2fc273919541
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2025-02-09 20:13:42 +1100

    Making data table from scratch

--- client-manager/src/renderer/src/pages/ClientForm.jsx
+++ client-manager/src/renderer/src/pages/ClientForm.jsx
@@ -1,18 +1,5 @@
-import { AllCommunityModule, ModuleRegistry, themeQuartz } from 'ag-grid-community'
-import { AgGridReact } from 'ag-grid-react'
-import { useCallback, useEffect, useState, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import EditButton from '../components/EditButton/EditButton'
-
-ModuleRegistry.registerModules([AllCommunityModule])
-
-const colDefs = [
-  { headerName: 'Name', field: 'client_name' },
-  { headerName: 'Parent', field: 'parent_name' },
-  { headerName: 'Address', field: 'client_address' },
-  { headerName: 'Contact', field: 'parent_phone' },
-  { headerName: 'School', field: 'client_school' },
-  { field: 'edit', cellRenderer: EditButton }
-]
 
 export default function ClientForm() {
   const [rowData, setRowData] = useState({})
@@ -22,38 +9,33 @@ export default function ClientForm() {
     data().then(setRowData)
   }, [])
 
-  const getRowId = useCallback((params) => String(params.data.id), [])
-
-  const defaultColDef = {
-    editable: false,
-    flex: 1,
-    minWidth: 100,
-    filter: true
-  }
-
-  const rowSelection = useMemo(() => {
-    return {
-      mode: 'multiRow',
-      checkboxes: true,
-      headerCheckbox: true,
-      enableClickSelection: true
-    }
-  }, [])
-
-  //   const rowSelection = () => {
-  //     return {
-  //       mode: 'multiRow'
-  //     }
-  //   }
+  // const thingo = rowData.map((client) => client.client_name)
+  // console.log(thingo)
 
   return (
-    <AgGridReact
-      theme={themeQuartz}
-      rowData={rowData}
-      columnDefs={colDefs}
-      defaultColDef={defaultColDef}
-      getRowId={getRowId}
-      rowSelection={rowSelection}
-    />
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Parent</th>
+          <th>Address</th>
+          <th>Contact</th>
+          <th>School</th>
+          <th>Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* {rowData.map((client) => console.log(client))} */}
+        {rowData.map((client) => (
+          <tr>
+            <td>{client.client_name}</td>
+            <td>{client.client_parent}</td>
+            <td>{client.client_address}</td>
+            <td>{client.parent_phone}</td>
+            <td>{client.client_school}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
