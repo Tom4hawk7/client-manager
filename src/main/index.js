28a19f2cb976df2669f575058695b39cc2f83f8a
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2025-04-24 12:47:47 +1000

    Changes applied to package.json

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import path, { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import { updateElectronApp } from 'update-electron-app'
+import { updateElectronApp, UpdateSourceType } from 'update-electron-app'
 
 updateElectronApp()
 
